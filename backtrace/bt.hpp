551938b31a8b67929ca69c6a733f3b17cfa8952f
rubicks <rubicks@users.noreply.github.com>
2013-11-29 15:24:24 -0500

    adding convenience lib for backtrace

---
+++ backtrace/bt.hpp
@@ -0,0 +1,14 @@
+/* cpp-crapola/backtrace/bt.hpp */
+
+
+#ifndef CPP_CRAPOLA___BACKTRACE___BT_HPP
+#define CPP_CRAPOLA___BACKTRACE___BT_HPP
+
+
+namespace bt
+{
+    int main( int argc, char**argv );
+}
+
+
+#endif//ndef CPP_CRAPOLA___BACKTRACE___BT_HPP
