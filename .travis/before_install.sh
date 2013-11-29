5009582c7a9cfb928bf48deab72b75791c54351d
rubicks <rubicks@users.noreply.github.com>
2013-11-29 13:51:34 -0500

    moving travis scripts around

---
+++ .travis/before_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# cpp-crapola/.travis/before_install.sh
+
+sudo apt-get update
---
+++ .travis/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# cpp-crapola/.travis/install.sh
+
+sudo apt-get install libboost-all-dev
