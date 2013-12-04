395574f1d88918a2c71a1bc4b239bc4a85b69daf
rubicks <rubicks@users.noreply.github.com>
2013-12-04 16:09:21 -0500

    allowing 1 week latency for apt-get update

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,4 +2,23 @@
 
 # cpp-crapola/.travis/before_install.sh
 
+duration="-1"
+
+seconds_since_last_modification()
+{
+    duration=$( expr $( date +%s ) - $( stat -c %Y ${@} ) )
+    return ${?}
+}
+
+seconds_since_last_modification /var/lib/apt/periodic/update-success-stamp
+
+# apt-get update (if it was run less than a week ago)
+if (( "${?}" == "0" ))
+then
+    if (( "${duration}" < "604800" ))
+    then
+        exit 0 ;
+    fi
+fi
+
 sudo apt-get update
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,6 +2,14 @@
 
 # cpp-crapola/.travis/install.sh
 
+seconds_since_last_modification()
+{
+    tmp=$( stat -c %Y ${@} )
+    echo "\${tmp} == \"${tmp}\""
+    # ret=`echo $(( $( date +%s ) - $( stat -c %Y ${@} ) ))`
+    # return ret ;
+}
+
 install()
 {
     for arg in ${@}
