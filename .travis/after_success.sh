5e23f75ca794941f15aaee89fd4eeba2fb4b3384
rubicks <rubicks@users.noreply.github.com>
2013-12-04 17:07:07 -0500

    attempting git checkout macosx from master build

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 # cpp-crapola/.travis/after_success.sh
+
+branch=$( git branch --list --no-color --no-column | awk '{print $2}' )
+
+if [[ "master" -ne "${branch}" ]]
+then
+    exit 0
+fi
+
+echo -n                 && \
+    git checkout macosx && \
+    git merge master    && \
+    git push            && \
+    git checkout master
