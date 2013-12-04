9dc61c907dc161f8947839d7637ec577cda1908d
rubicks <rubicks@users.noreply.github.com>
2013-12-04 16:50:05 -0500

    removing dead code from .travis/install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,14 +2,6 @@
 
 # cpp-crapola/.travis/install.sh
 
-seconds_since_last_modification()
-{
-    tmp=$( stat -c %Y ${@} )
-    echo "\${tmp} == \"${tmp}\""
-    # ret=`echo $(( $( date +%s ) - $( stat -c %Y ${@} ) ))`
-    # return ret ;
-}
-
 install()
 {
     for arg in ${@}
