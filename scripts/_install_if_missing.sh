e2d2eb51f96af08e6ccbf09aae520c21f3117fda
rubicks <rubicks@users.noreply.github.com>
2013-12-12 15:57:27 -0500

    first attempt at cross-platform package scripts

--- scripts/_install_if_missing.sh
+++ scripts/_install_if_missing.sh
@@ -5,12 +5,28 @@
 source ${PROJECT_DIR}/scripts/_noisy.sh     || exit 1
 source ${PROJECT_DIR}/scripts/_do_or_die.sh || exit 1
 
+
+case "${_system_name}" in
+    Ubuntu)
+        query_cmd="dpkg-query --status"
+        install_cmd="sudo apt-get --assume-yes install"
+        ;;
+    OSX)
+        query_cmd="brew list"
+        install_cmd="brew install"
+        ;;
+    *)
+        echo "environment variable \${_system_name} has undefined value \"${_system_name}\""
+        exit 1
+esac
+
+
 function _install_if_missing()
 {
     for arg in ${@}
     do
-        _noisy dpkg-query --status ${arg} || \
-            _do_or_die sudo apt-get --assume-yes install ${arg}
+        _noisy ${query_cmd} ${arg} || \
+            _do_or_die ${install_cmd} ${arg}
     done
 }
 
