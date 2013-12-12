7bfb860deb3bd2c6dedced50202040a4bf4da143
rubicks <rubicks@users.noreply.github.com>
2013-12-12 16:46:39 -0500

    adding git config stuff to .travis/after_success

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -10,6 +10,9 @@
 
 source ${PROJECT_DIR}/scripts/_do_or_die.sh || exit 1
 
+[ "`git config user.name`" ]  || _do_or_die git config user.name "`whoami`"
+[ "`git config user.email`" ] || _do_or_die git config user.name "`whoami`@`uname -n`"
+
 _do_or_die git checkout macosx
 _do_or_die git merge --no-edit master
 _do_or_die git push
