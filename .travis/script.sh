cffccce996607624d521154c4a3bb7151e15d9a3
rubicks <rubicks@users.noreply.github.com>
2013-12-12 15:26:31 -0500

    darwin mkdir no like long args

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,9 +5,9 @@
 source ${PROJECT_DIR}/scripts/_do_or_die.sh || exit 1
 
 echo -n                                                          && \
-    _noisy mkdir --verbose --parents ${PROJECT_DIR}/m4           && \
+    _noisy mkdir -vp ${PROJECT_DIR}/m4                           && \
     _noisy autoreconf --verbose --force --install ${PROJECT_DIR} && \
-    _noisy mkdir --verbose --parents ${PROJECT_DIR}/build        && \
+    _noisy mkdir -vp ${PROJECT_DIR}/build                        && \
     _noisy cd ${PROJECT_DIR}/build                               && \
     _noisy ${PROJECT_DIR}/configure --verbose                    && \
     _noisy make --debug                                          && \
