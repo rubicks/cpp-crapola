f97b5d09571d67397231fbc528b6ba342e41fcf9
rubicks <rubicks@users.noreply.github.com>
2013-12-13 12:20:01 -0500

    hardcoding git config user name and email

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -11,8 +11,8 @@
 
 source ${PROJECT_DIR}/scripts/_do_or_die.sh || exit 1
 
-_do_or_die git config user.name  ${GIT_NAME}
-_do_or_die git config user.email ${GIT_EMAIL}
+_do_or_die git config user.name  "Travis CI"
+_do_or_die git config user.email "`whoami`@`uname -n`"
 _do_or_die git checkout macosx
 _do_or_die git merge --no-edit master
 _do_or_die git push
