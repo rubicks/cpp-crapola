261541236691d702ea136bd1c3763302b7104ead
rubicks <rubicks@users.noreply.github.com>
2013-12-13 13:36:12 -0500

    noisy diagnostics for after_success.sh

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,16 +2,31 @@
 
 # cpp-crapola/.travis/after_success.sh
 
-[ "${TRAVIS_JOB_ID}" ] || \
-    echo "\${TRAVIS_JOB_ID} == \"${TRAVIS_JOB_ID}\"" && \
-    exit 0
-# only travis does the following (automate the merge!)
-
-[ "master" == "${TRAVIS_BRANCH}" ] || exit 0
-# build master -> merge to macosx
-
+source ${PROJECT_DIR}/scripts/_disp.sh      || exit 1
 source ${PROJECT_DIR}/scripts/_do_or_die.sh || exit 1
 source ${PROJECT_DIR}/scripts/_merge.sh     || exit 1
+
+_disp TRAVIS_COMMIT TRAVIS_BRANCH
+
+if [ "${TRAVIS_COMMIT}" ]
+then
+    echo "TRAVIS_COMMIT defined"
+else
+    echo "TRAVIS_COMMIT undefined"
+    exit 0
+fi
+    
+if [ "master" == "${TRAVIS_BRANCH}" ]
+then
+    echo "TRAVIS_BRANCH is master"
+else
+    echo "TRAVIS_BRANCH is not master"
+    exit 0
+fi
+
+
+# only travis does the following (automate the merge!)
+
 
 [ "`git config user.name`" ]  || \
     _do_or_die git config user.name  "Travis CI"
